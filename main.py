7f0c348bb72f26d3fd61d4fa35b32c1d3d32f922
Lucas Schoubye <lucasschoubye@gmail.com>
2023-06-12 16:23:38 +0200

    Setup multithreaded

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+import threading
+
 import Course
 import Robot
 import Controller
 import openCV
 
 def main():
-    
-    # OpenCV script
-    openCV.main.main()
-    
-    # Run algorithm
-    Controller.run()
+
+    #Setup multithreading
+    ball_positions = list()
+
+    lock = threading.Lock()
+
+    def openCVMultithreaded():
+        global ball_positions
+
+        with lock:
+            #ball_positions
+            openCV.main.main()
+    def controllerMultithreaded():
+        global ball_positions
+
+        with lock:
+            #ball_positions
+            Controller.run()
+
+    # Setup OpenCV an algorithm
+    thread_1 = threading.Thread(target=openCVMultithreaded())
+    thread_2 = threading.Thread(target=controllerMultithreaded())
+
+    # Start Threads
+    thread_1.start()
+    thread_2.start()
 
 
 if __name__ == '__main__':
